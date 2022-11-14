54e362b81cad4ee6eda32eb7cb39def3adcd6840
Jingyu Pang <jingyupang@JingyudeMacBook-Pro.local>
2022-11-14 17:29:30 -0500

    serve the static file use express

---
+++ slack.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const socketio = require("socket.io");
+
+const app = express();
+app.use(express.static(__dirname + "/public"));
+const expressServer = app.listen("9000");
